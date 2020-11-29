b4f25ecf58cfcf39392fee93bcb0ce12121872b2
Rohan Bongurala <rbongur1@binghamton.edu>
2020-11-29 14:01:39 -0600

    added check winner utility to check if winner exists-- tests pass

--- tic-tac-toe/src/components/board/board.test.js
+++ tic-tac-toe/src/components/board/board.test.js
@@ -1,6 +1,7 @@
 import { shallow } from 'enzyme';
 import Square from '../square/square';
 import Board from './board';
+import { checkWinner } from '../../utilities/checkWinner';
 
 describe('test board component', () => {
   it('render board and check for 9 squares', () => {
@@ -14,14 +15,14 @@ describe('test board component', () => {
     expect(instance.state.squares[0]).toBe(null);
     expect(instance.state.squares.length).toBe(9);
     instance.state.squares.every(element => expect(element).toBe(null));
-  })
+  });
 
   it('click square to mark it with X on callback', () => {
     const wrapper = shallow(<Board />);
     const instance = wrapper.instance();
     wrapper.find(Square).at(1).simulate('click');
     expect(instance.state.squares.some(element => element === 'X')).toBeTruthy();
-  })
+  });
 
   it('alternative X and O when click on square', () => {
     const wrapper = shallow(<Board />);
@@ -30,14 +31,25 @@ describe('test board component', () => {
     wrapper.find(Square).at(5).simulate('click');
     expect(instance.state.squares[1]).toBe('X');
     expect(instance.state.squares[5]).toBe('O');
-  })
+  });
 
   it('show the current player to make move', () => {
     const wrapper = shallow(<Board />);
     expect(wrapper.find('div.status').text()).toMatch(/Player to make move is/);
-  })
+  });
 
-  it('check for a winning pattern', () => {
+  it('check for a winning pattern and true', () => {
+    const wrapper = shallow(<Board />);
+    const instance = wrapper.instance();
+    wrapper.find(Square).at(0).simulate('click');
+    wrapper.find(Square).at(5).simulate('click');
+    wrapper.find(Square).at(1).simulate('click');
+    wrapper.find(Square).at(4).simulate('click');
+    wrapper.find(Square).at(2).simulate('click');
+    expect(checkWinner(instance.state.squares)).toBeTruthy();
+  });
+
+  it('check for a winning pattern and false if no combination', () => {
     const wrapper = shallow(<Board />);
     const instance = wrapper.instance();
     wrapper.find(Square).at(1).simulate('click');
@@ -45,7 +57,7 @@ describe('test board component', () => {
     wrapper.find(Square).at(2).simulate('click');
     wrapper.find(Square).at(4).simulate('click');
     wrapper.find(Square).at(3).simulate('click');
-    expect(checkWinner(instance.state.squares)).toBeTruthy();
-  })
+    expect(checkWinner(instance.state.squares)).toBeFalsy();
+  });
 
 });
