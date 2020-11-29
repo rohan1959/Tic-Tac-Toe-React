dc50f3f4f99af922194f74c11886c87d172a304c
Rohan Bongurala <rbongur1@binghamton.edu>
2020-11-29 11:29:02 -0600

    test fail for add click for button

--- tic-tac-toe/src/components/square/square.test.js
+++ tic-tac-toe/src/components/square/square.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import Square from './square';
+import { shallow } from 'enzyme';
 
-test('render square with button with value as X', () => {
-  render(<Square />);
-  const linkElement = screen.getByText('X');
-  expect(linkElement).toBeInTheDocument();
+
+describe('test square component', () => {
+
+  test('render square with button with value no value initially', () => {
+    render(<Square />);
+    const linkElement = screen.getByText('X');
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test('button to have on click property', () => {
+    const mockCallBack = jest.fn();
+    const button = shallow(<Square />);
+    button.find('.square').simulate('click');
+    expect(mockCallBack.mock.calls.length).toEqual(1);
+  })
 });
+
