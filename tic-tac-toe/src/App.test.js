4a66aa7446593f36b4f52b3140aed6c696f0a29a
Rohan Bongurala <rbongur1@binghamton.edu>
2020-11-29 09:58:32 -0600

    add header tic tac toe

--- tic-tac-toe/src/App.test.js
+++ tic-tac-toe/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header with Lets Play Tic Tac Toe', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Tic Tac Toe/i);
   expect(linkElement).toBeInTheDocument();
 });
