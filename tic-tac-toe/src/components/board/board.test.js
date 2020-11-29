88b009e4e7bd13a54bf519d709dab5321296b462
Rohan Bongurala <rbongur1@binghamton.edu>
2020-11-29 10:38:18 -0600

    added enzyme and board component to have 9 squares

---
+++ tic-tac-toe/src/components/board/board.test.js
@@ -0,0 +1,16 @@
+import { shallow } from 'enzyme';
+import Square from '../square/square';
+import Board from './board';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+describe('test board component', () => {
+
+  it('render board and check for 9 squares', () => {
+    const wrapper = shallow(<Board />);
+    expect(wrapper.find(Square)).to.have.lengthOf(9);
+  });
+});
