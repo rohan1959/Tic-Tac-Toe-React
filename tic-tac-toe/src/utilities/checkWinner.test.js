ab786f4d4531566ef0c7b8d475016d98df1ddc04
Rohan Bongurala <rbongur1@binghamton.edu>
2020-11-29 19:21:19 -0600

    added test cases for utility func

---
+++ tic-tac-toe/src/utilities/checkWinner.test.js
@@ -0,0 +1,14 @@
+import { checkWinner } from './checkWinner';
+
+describe('validate check winner function', () => {
+  it('check winner returns true when winner pattern exists', () => {
+    const squares = ['X', 'O', 'X', 'O', 'X', 'X', 'O', 'O', 'X'];
+    expect(checkWinner(squares)).toBeTruthy();
+  });
+
+  it('check winner returns false when winner pattern exists', () => {
+    const squares = ['X', 'O', 'X', 'O', 'X', 'X', 'O', 'X', 'O'];
+    expect(checkWinner(squares)).toBeFalsy();
+  });
+
+});
