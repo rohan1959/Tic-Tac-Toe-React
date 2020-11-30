2c9662986162f3803633a44e3e5818411110fcb0
Rohan Bongurala <rbongur1@binghamton.edu>
2020-11-29 19:23:56 -0600

    corrected typo

--- tic-tac-toe/src/utilities/checkWinner.test.js
+++ tic-tac-toe/src/utilities/checkWinner.test.js
@@ -6,7 +6,7 @@ describe('validate check winner function', () => {
     expect(checkWinner(squares)).toBeTruthy();
   });
 
-  it('check winner returns false when winner pattern exists', () => {
+  it('check winner returns false when winner pattern doesnot exists', () => {
     const squares = ['X', 'O', 'X', 'O', 'X', 'X', 'O', 'X', 'O'];
     expect(checkWinner(squares)).toBeFalsy();
   });
